2282b35d794b2b49678c9ce10ca6e1fae79d6d28
guogai1949 <578538991@qq.com>
2018-10-23 09:32:04 +0800

    2018/10/23

--- src/main/java/com/js/worker/code/Test8.java
+++ src/main/java/com/js/worker/code/Test8.java
@@ -9,12 +9,31 @@ import java.util.Map;
 public class Test8 {
 	
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		Iterator<String> iterator = list.iterator();
-		if(iterator.hasNext()) {
-			String s = iterator.next();
-			iterator.remove();
-		}
+		Integer a = 100;
+		Integer b = 130;
+		Integer c = new Integer(100);
+		int d = 100;
+		Integer e = Integer.valueOf(100);
+		Integer f = 130;
+		System.out.println("==============test1");
+		System.out.println("a == c :" + (a == c));
+		System.out.println("a.equals(c) :" + a.equals(c));
+		
+		System.out.println("==============test2");
+		System.out.println("b == f :" + (b == f));
+		System.out.println("b.equals(f) :" + b.equals(f));
+		
+		System.out.println("==============test3");
+		System.out.println("a == d :" + (a == d));
+		System.out.println("a.equals(d) :" + a.equals(d));
+		
+		System.out.println("==============test4");
+		System.out.println("a == e :" + (a == e));
+		System.out.println("a.equals(e) :" + a.equals(e));
+		
+		System.out.println("==============test5");
+		System.out.println("c == d :" + (c == d));
+		System.out.println("c.equals(d) :" + c.equals(d));
 	}
 
 }
